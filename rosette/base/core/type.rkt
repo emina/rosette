8212a96d37ddec17d5c287d95199625f92e17b1f
Emina Torlak <emina@alum.mit.edu>
2015-10-21 15:52:55 -0700

    Remove dead code from type.rkt

--- rosette/base/core/type.rkt
+++ rosette/base/core/type.rkt
@@ -14,7 +14,7 @@
  least-common-supertype 
  cast subtype?
  
- type-of @any/c lift-type lifted-type define-lifted-type)
+ type-of @any/c lifted-type define-lifted-type)
 
 #|-----------------------------------------------------------------------------------|#
 ; The type generic interface defines a symbolic type.  Each value has a type.  Structures that 
@@ -126,71 +126,3 @@
 (define type-of 
   (typechecker 
    #:base boolean? number? list? pair? procedure? vector? box?))
-
-
-
-
-#|-----------------------------------------------------------------------------------|#
-; Rosette types that lift built-in Racket types are constructed using the lift-type procedure. 
-; In the current type system, the following types are expected to be lifted: 
-; boolean?, number?, list?, pair?, procedure?, vector?, box?
-; See lift-type and type-of for details.
-#|-----------------------------------------------------------------------------------|#
-
-(struct base-type 
-  (name pred applicable? eq? equal?          
-   lcs cast compress construct deconstruct)    
-  #:property prop:procedure 
-  (struct-field-index pred)
-  #:methods gen:type
-  [(define (least-common-supertype t0 t1) ((base-type-lcs t0) t1))
-   (define (type-name type)               (base-type-name type))
-   (define (type-applicable? type)        (base-type-applicable? type))
-   (define (type-eq? type u v)            ((base-type-eq? type) u v))   
-   (define (type-equal? type u v)         ((base-type-equal? type) u v))   
-   (define (cast type v)                  ((base-type-cast type) v))
-   (define (type-compress type force? ps) ((base-type-compress type) force? ps))
-   (define (type-construct type vals)     ((base-type-construct type) vals))
-   (define (type-deconstruct type val)    ((base-type-deconstruct type) val))]
-  #:methods gen:custom-write
-  [(define (write-proc self port mode)
-     (fprintf port "~a" (base-type-name self)))])
-
-(define (no-compress force? ps) ps)
-
-; Constructs a new lifted type for the given Racket built-in type, using the 
-; following argumets:
-;   base                             ; Racket type being lifted.
-;   #:is-a? is-a?                    ; Predicate that recognizes concrete and symbolic values of the lifted type.
-;   #:eq?  eq?                       ; eq? predicate for the lifted type
-;   #:equal? equal?                  ; equal? predicate for the lifted type
-;   #:least-common-supertype lcs     ; Procedure for computing the least common supertype of this and the given type.
-;   #:cast cas                       ; Procedure for casting a value to this lifted type.
-;   #:applicable? [app? #f]          ; Boolean flag indicating if this type represents applicable values.
-;   #:compress [compress #f]         ; Procedure for custom [eager] merging of values of this type.
-;   #:construct [construct car]      ; Procedure that constructs a value of this type from a list of arguments.
-;   #:deconstruct [deconstruct list] ; Procedure that decomposes a value of this type to a list of constituent values.
-; A given Racket type cannot be lifted more than once.  That is, multiple attempts to 
-; call lift-type with the same base type as argument will result in an error.
-; Only these Racket types are expected to be lifted:
-; boolean?, number?, list?, pair?, procedure?, vector?, and box?.
-(define (lift-type base
-                   #:is-a? is-a? 
-                   #:eq? is-eq? 
-                   #:equal? is-equal? 
-                   #:least-common-supertype lcs
-                   #:cast cast 
-                   #:applicable? [applicable? #f]
-                   #:compress [compress #f]
-                   #:construct [construct car]
-                   #:deconstruct [deconstruct list])
-  (unless (hash-has-key? types base)
-    (error 'lift "Cannot lift ~a.\nExpected one of ~a." base (hash-keys types)))
-  (unless (eq? @any/c (hash-ref types base))
-    (error 'lift "Type already lifted: ~a." base))
-  (define lifted 
-    (base-type (object-name base) is-a? applicable? is-eq? is-equal? 
-               lcs cast (or compress no-compress) construct deconstruct))
-  (hash-set! types base lifted)
-  lifted)
-
