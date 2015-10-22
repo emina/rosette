320c9977d42c214fb679704edfb92da40c1ded56
Emina Torlak <emina@alum.mit.edu>
2015-10-22 12:49:37 -0700

    Factor out the loop-free fragment synthesizer from the define-fragment form.

--- sdsl/bv/lang/synthesizer.rkt
+++ sdsl/bv/lang/synthesizer.rkt
@@ -9,7 +9,9 @@
          rosette/base/util/log
          (for-syntax racket/syntax))
 
-(provide define-fragment)
+(provide define-fragment synthesize-fragment)
+
+(define verbose? (make-parameter #t))
 
 (current-log-handler (log-handler #:info (curry equal? 'define-fragment)))
 
@@ -23,20 +25,34 @@
      #'(define-fragment (id param ...) #:min-bitwidth k #:requires (const #t) #:ensures post #:library lib-expr)]
     [(_ (id param ...) #:min-bitwidth k #:requires pre #:ensures post #:library lib-expr )
      #`(define-values (id #,(format-id #'id "~a-stx" #'id #:source #'id)) 
-         (parameterize ([current-oracle (oracle)]
-                        [current-solution (empty-solution)]
-                        [current-log-source 'define-fragment]
-                        [inputs #,(length (syntax->list #'(param ...)))])
-           (log-info "synthesizing ~a" #'id)
-           (define-symbolic* param @number?) ...
-           (define lib (optimize-library #:inputs (list param ...) #:library lib-expr))
-           (define-values (val cpu real gc) 
-             (time-apply ex-all-solve (list (list param ...) pre post lib k)))
-           (define fun-stx (fragment->syntax #:solution (current-solution) 
-                                             #:inputs (list (quote-syntax param) ...)
-                                             #:library lib))
-           (log-info "synthesized ~a (ms): cpu = ~a, real = ~a, gc = ~a" #'id cpu real gc)
-           (values (procedure-rename (eval fun-stx) 'id) fun-stx)))]))
+         (synthesize-fragment (id param ...) #:min-bitwidth k #:requires pre #:ensures post #:library lib-expr))]))
+
+; Returns two values:  a procedure named id and a syntactic representation of that procedure.
+; The procedure's body is a loop-free code fragment, built from the provided library of components,
+; that satisfies the give post condition on all inputs that satisfy the given pre-condition.
+(define-syntax (synthesize-fragment stx)
+  (syntax-case stx ()
+    [(_ (id param ...) #:ensures post #:library lib-expr)
+     #'(synthesize-fragment (id param ...) #:requires (const #t) #:ensures post #:library lib-expr)]
+    [(_ (id param ...) #:requires pre #:ensures post #:library lib-expr)
+     #'(synthesize-fragment (id param ...) #:min-bitwidth 4 #:requires pre #:ensures post #:library lib-expr)]
+    [(_ (id param ...) #:min-bitwidth k #:ensures post #:library lib-expr)
+     #'(synthesize-fragment (id param ...) #:min-bitwidth k #:requires (const #t) #:ensures post #:library lib-expr)]
+    [(_ (id param ...) #:min-bitwidth k #:requires pre #:ensures post #:library lib-expr )
+     #`(parameterize ([current-oracle (oracle)]
+                      [current-solution (empty-solution)]
+                      [current-log-source (if (verbose?) 'define-fragment #f)]
+                      [inputs #,(length (syntax->list #'(param ...)))])
+         (log-info "synthesizing ~a" #'id)
+         (define-symbolic* param @number?) ...
+         (define lib (optimize-library #:inputs (list param ...) #:library lib-expr))
+         (define-values (val cpu real gc) 
+           (time-apply ex-all-solve (list (list param ...) pre post lib k)))
+         (define fun-stx (fragment->syntax #:solution (current-solution) 
+                                           #:inputs (list (quote-syntax param) ...)
+                                           #:library lib))
+         (log-info "synthesized ~a (ms): cpu = ~a, real = ~a, gc = ~a" #'id cpu real gc)
+         (values (procedure-rename (eval fun-stx) 'id) fun-stx))]))
 
 (define (optimize-library #:inputs inputs #:library lib)
   (let ([consts (for/list ([inst lib] #:when (null? (instruction-inputs inst))) inst)])
