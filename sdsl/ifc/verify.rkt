230d4216bc0f1a6f4891d2326507d021199a77aa
Emina Torlak <emina@alum.mit.edu>
2015-10-22 21:36:58 -0700

    Refactor program creation and verification so that all temporary symbolic constants and terms are cleared when the verifier returns.

--- sdsl/ifc/verify.rkt
+++ sdsl/ifc/verify.rkt
@@ -2,7 +2,7 @@
 
 (require "machine.rkt" "indistinguishable.rkt")
 
-(provide verify-EENI)
+(provide verify-EENI verify-EENI* (struct-out EENI-witness))
 
 ; Verifies the EENI[start, end, ≈] property for machines executing 
 ; k steps of a program that is indistinguishable from the given program.  
@@ -12,35 +12,79 @@
 ; indistinguishable. The end argument is a predicate on machines, and 
 ; ≈ is an indistinguishability relation on machines.  The length 
 ; of the program list is used as the default value for the number of steps.
-; (-> (-> (listof instruction?) machine?)
+; (-> (-> (-> () (listof instruction?)) machine?)
 ;     (-> machine? boolean?)
 ;     (-> machine? machine? boolean?)
 ;     (listof instruction?)
-;     [number?]
+;     [(or/c number? #f)]
 ;     void?)
-(define (verify-EENI start end ≈ prog [k (length prog)])
-  (printf "\n-------Verify EENI[~a, ~a, ~a] for ~a steps of ~s-------\n"
-          (object-name start) (object-name end) (object-name ≈) k (format-program prog))
-  
-  (define t0 (current-milliseconds))
+(define (verify-EENI start end ≈ prog [k #f])
+  (time (displayln (verify-EENI* start end ≈ prog k #t))))
 
-  (define m0 (start prog))
-  (define m1 (start (map instruction (map procedure prog)))) ; Use a fresh program (same procedures, fresh args).
-  
-  (define cex 
-    (with-handlers ([exn:fail? (lambda (e) #f)]) ; The verify form throws an exception if no cex is found.
-      (verify 
-       #:assume  (begin 
-                   (assert (≈ m0 m1))     
-                   (set! m0 (step m0 k))  
-                   (set! m1 (step m1 k)) 
-                   (assert (end m0))
-                   (assert (end m1)))
-       #:guarantee (assert (≈ m0 m1)))))
-  
-  (define t (- (current-milliseconds) t0))
-  (print-cex cex t (cons "m0" m0) (cons "m1" m1)))
-               
+; Verifies the EENI[start, end, ≈] property for machines executing 
+; k steps of a program that is indistinguishable from the given program.  
+; The start argument is a procedure that takes as input a program and 
+; returns a fresh machine m initialized with that program.  The pc, 
+; stack and memory of every machine m0 and m1 returned by start must be 
+; indistinguishable. The end argument is a predicate on machines, and 
+; ≈ is an indistinguishability relation on machines.  The length 
+; of the program list is used as the default value for the number of steps.
+; The procedure returns a counterexample, if any, that the EENI[start, end, ≈] 
+; property is violated by producing an EENI-witness.  If there are no 
+; counterexample, the procedure returns #t.
+; (-> (-> (-> () (listof instruction?)) machine?)
+;     (-> machine? boolean?)
+;     (-> machine? machine? boolean?)
+;     (listof instruction?)
+;     [(or/c number? #f)]
+;     (or/c EENI-witness? #t))
+(define (verify-EENI* start end ≈ prog [k #f] [verbose? #f])
+  (parameterize ([current-solution (empty-solution)]
+                 [current-oracle (oracle)]
+                 [term-cache (hash-copy (term-cache))])
+    
+    (define p (prog))
+    
+    (unless k 
+      (set! k (length p)))
+    
+    (when verbose?
+      (printf "\n-------Verify EENI[~a, ~a, ~a] for ~a steps of ~s-------\n"
+              (object-name start) (object-name end) (object-name ≈) k (format-program p)))
+
+    (define m0 (start p))
+    (define m1 (start (map instruction (map procedure p)))) ; Use a fresh program (same procedures, fresh args).
+    
+    (define cex
+      (let ([m0 m0]
+            [m1 m1])
+        (with-handlers ([exn:fail? (lambda (e) #f)]) ; The verify form throws an exception if no cex is found.
+          (verify 
+           #:assume  (begin 
+                       (assert (≈ m0 m1))     
+                       (set! m0 (step m0 k))  
+                       (set! m1 (step m1 k)) 
+                       (assert (end m0))
+                       (assert (end m1)))
+           #:guarantee (assert (≈ m0 m1))))))
+    
+    (if cex (EENI-witness (evaluate m0 cex) (evaluate m1 cex) k ≈) #t)))
+
+(struct EENI-witness (m0 m1 k ≈)
+  #:transparent 
+  #:guard (lambda (m0 m1 k ≈ id)
+            (unless (and (≈ m0 m1) (not (≈ (step m0 k) (step m1 k))))
+              (error id "Not a valid EENI witness: ~a, ~a, ~a, ~a." m0 m1 k ≈))
+            (values m0 m1 k ≈))
+  #:methods gen:custom-write
+  [(define (write-proc self port mode)
+     (match-let ([(EENI-witness m0 m1 k _) self])
+        (fprintf port "m0 initial:\n~s\n" m0)
+        (fprintf port "m0 final:\n~s\n" (step m0 k))
+        (fprintf port "m1 initial:\n~s\n" m1)
+        (fprintf port "m1 final:\n~s\n" (step m1 k))))])
+        
+     
 ; Returns a printable representation of the given program.
 (define (format-program prog)
   (match prog
@@ -48,12 +92,3 @@
      (sort (map object-name (union-values r)) string<? #:key symbol->string)]
     [(list (instruction (? procedure? proc) _) ...) 
      (map object-name proc)]))
-
-; Prints the given counterexample for the specified label/machine pairs.
-(define (print-cex cex t . labeled-machines)
-  (cond [cex  (printf "Counterexample found (~a ms).\n" t)
-              (for ([label/machine labeled-machines])
-                (printf "~a:\n~s\n" (car label/machine) (evaluate (cdr label/machine) cex)))]
-        [else (printf "No counterexample found (~a ms).\n" t)]))
-
-
