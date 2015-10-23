b7ee37842d7c871a7609fd7d7595dd1d6b40b8a1
Emina Torlak <emina@alum.mit.edu>
2015-10-22 22:52:04 -0700

    Consolidate all SDSL tests into a single top-level file.

---
+++ test/all-sdsl-tests.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+;; ----------- WebSynth ----------- ;; 
+(require "../sdsl/websynth/test/itunes100_2.rkt")
+(require "../sdsl/websynth/test/itunes100_4.rkt")
+(require "../sdsl/websynth/test/itunes100_8.rkt")
+(require "../sdsl/websynth/test/itunes100_16.rkt")
+
+(require "../sdsl/websynth/test/imdb250_2.rkt")
+(require "../sdsl/websynth/test/imdb250_4.rkt")
+(require "../sdsl/websynth/test/imdb250_8.rkt")
+(require "../sdsl/websynth/test/imdb250_16.rkt")
+
+(require "../sdsl/websynth/test/alanon_arkansas_2.rkt")
+(require "../sdsl/websynth/test/alanon_arkansas_4.rkt")
+(require "../sdsl/websynth/test/alanon_arkansas_8.rkt")
+(require "../sdsl/websynth/test/alanon_arkansas_16.rkt")
+
+;; ----------- BV ----------- ;; 
+(require "../sdsl/bv/test/easy.rkt")
+(require "../sdsl/bv/test/medium.rkt")
+
+;; ----------- IFC ----------- ;; 
+(require "../sdsl/ifc/test.rkt")
--- test/sdsl/bv.rkt
+++
@@ -1,4 +0,0 @@
-#lang racket
-
-(require "../../sdsl/bv/test/easy.rkt")
-(require "../../sdsl/bv/test/medium.rkt")
