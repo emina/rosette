986ddcd01708f5a3916d2c76bf36811a9b37efe6
Emina Torlak <emina@alum.mit.edu>
2015-10-23 11:15:34 -0700

    Collect all websynth tests into a single file.

---
+++ sdsl/websynth/test/all-tests.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+;; ----------- WebSynth ----------- ;; 
+(require "itunes100_2.rkt")
+(require "itunes100_4.rkt")
+(require "itunes100_8.rkt")
+(require "itunes100_16.rkt")
+
+(require "imdb250_2.rkt")
+(require "imdb250_4.rkt")
+(require "imdb250_8.rkt")
+(require "imdb250_16.rkt")
+
+(require "alanon_arkansas_2.rkt")
+(require "alanon_arkansas_4.rkt")
+(require "alanon_arkansas_8.rkt")
+(require "alanon_arkansas_16.rkt")
