3ac38b3312cff74b6627637ae554f8292dd0bcc0
Emina Torlak <emina@alum.mit.edu>
2015-10-24 13:04:42 -0700

    Automated tests for SynthCL.

--- test/all-sdsl-tests.rkt
+++ test/all-sdsl-tests.rkt
@@ -2,9 +2,11 @@
 
 (define-syntax-rule (module@ id)
   (module+ id
-    (require "../sdsl/websynth/test/all-tests.rkt")  ; WebSynth
-    (require "../sdsl/bv/test/all-tests.rkt")        ; BV
-    (require (submod "../sdsl/ifc/test.rkt" id))))   ; IFC tests
+    (require "../sdsl/websynth/test/all-tests.rkt")            ; WebSynth
+    (require "../sdsl/bv/test/all-tests.rkt")                  ; BV
+    (require (submod "../sdsl/ifc/test.rkt" id))               ; IFC tests
+    (require (submod "../sdsl/synthcl/test/all-tests.rkt" id)) ; SynthCL tests
+    ))   
 
 (module@ test) ; All tests
 (module@ fast) ; Fast tests
