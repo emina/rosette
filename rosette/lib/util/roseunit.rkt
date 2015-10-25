3344393d53b2d4e7beffd74206e5ae4a384031d6
Emina Torlak <emina@alum.mit.edu>
2015-10-24 17:16:40 -0700

    Amend roseunit to provide a run-all-tests form, and also modify test-suite+ so that it allows shared Rosette state between test suites in the same module.

--- rosette/lib/util/roseunit.rkt
+++ rosette/lib/util/roseunit.rkt
@@ -2,23 +2,42 @@
 
 ; Utilities for testing Rosette programs.
 (require rackunit)
-(require (only-in rosette clear-state! current-bitwidth solution? sat? unsat?))
+(require (only-in rosette 
+                  clear-state!
+                  current-bitwidth term-cache current-oracle oracle with-asserts-only
+                  current-solution empty-solution solution? sat? unsat?))
 
-(provide test-suite+ test-sat test-unsat check-sat check-unsat)
+(provide run-all-tests test-suite+ test-sat test-unsat check-sat check-unsat)
 
-; Makes sure that a test suite clear all Rosette state after it terminates.
+; Given a set of relative paths containing modules with tests, 
+; requires them all into the present environment, one by one, 
+; clearing the Rosette state between each import.
+(define-syntax (run-all-tests stx)
+  (syntax-case stx ()
+    ([_ path ...]
+     (with-syntax ([(id ...) (generate-temporaries #'(path ...))])
+       (syntax/loc stx
+         (begin
+           (module id racket 
+             (require path)
+             (require (only-in rosette/safe clear-state!))
+             (clear-state!)) ...
+            (require 'id) ...))))))
+
+; Makes sure that a test suite clears all Rosette state after it terminates.
 (define-syntax test-suite+
   (syntax-rules ()
     [(_ name #:before before #:after after test ...)
      (test-suite 
       name
-      #:before (thunk 
-                (printf "~a\n" name)  
-                (before)) 
-      #:after (thunk 
-               (after) 
-               (clear-state!)) 
-      test ...)]
+      #:before (thunk (printf "~a\n" name) (before)) 
+      #:after after
+      (with-asserts-only
+       (parameterize ([current-bitwidth (current-bitwidth)]
+                      [term-cache (hash-copy (term-cache))]
+                      [current-oracle (oracle (current-oracle))]
+                      [current-solution (empty-solution)])
+         test ...)))]
     [(_ name #:before before test ...)
      (test-suite+ name #:before before #:after void test ...)]
     [(_ name #:after after test ...)
