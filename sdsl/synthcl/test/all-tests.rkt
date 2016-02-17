f6f18433ef239faf2363addfc4e4c1804a8a83cc
Emina Torlak <emina@alum.mit.edu>
2016-02-16 16:44:47 -0800

    Checkpoint: porting SynthCL to new Rosette (examples/matrixMultiply)

--- sdsl/synthcl/test/all-tests.rkt
+++ sdsl/synthcl/test/all-tests.rkt
@@ -8,8 +8,8 @@
  "reals.rkt"
  "typecheck.rkt"
  "work.rkt"
- ;(submod "../examples/matrixMultiply/synth/test.rkt")
- ;(submod "../examples/matrixMultiply/verify/test.rkt")
+ (submod "../examples/matrixMultiply/synth/test.rkt")
+ (submod "../examples/matrixMultiply/verify/test.rkt")
  ;(submod "../examples/sobelFilter/test.rkt")
  (submod "../examples/fastWalshTransform/synth/test.rkt")
  (submod "../examples/fastWalshTransform/verify/test.rkt")
