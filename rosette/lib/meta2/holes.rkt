fc50879d16479a0f0526687718e9ce4cdfbbde34
Emina Torlak <emina@alum.mit.edu>
2016-02-25 09:35:29 -0800

    Add print-forms utility to new define-synthax implementation.

--- rosette/lib/meta2/holes.rkt
+++ rosette/lib/meta2/holes.rkt
@@ -2,6 +2,10 @@
 
 (require "core.rkt" "form.rkt")
 
-(provide solution->forms
-         define-synthax 
-         ?? choose)
+(provide solution->forms print-forms
+         define-synthax ?? choose)
+
+(define (print-forms sol)
+  (for ([f (solution->forms sol)])
+    (printf "~a:~a:~a\n" (syntax-source f) (syntax-line f) (syntax-column f))
+    (printf "~a\n" (pretty-format (syntax->datum f)))))
