af8abc6a7ccd508d4d52fcb6fe9fd434d03648f6
Emina Torlak <emina@alum.mit.edu>
2016-03-02 14:35:07 -0800

    Simplify operator implementation in preparation for adding uninterpreted functions.

--- rosette/base/core/op.rkt
+++ rosette/base/core/op.rkt
@@ -11,22 +11,16 @@
 ; internally by Rosette for efficiency.  It assumes that all of its arguments are 
 ; properly typed and that all preconditions are met.
 (struct op 
-  (name h1 h2 safe unsafe type)  
+  (name safe unsafe type)  
   #:property prop:procedure 
   (struct-field-index safe)
   #:methods gen:custom-write
-  [(define (write-proc self port mode) (fprintf port "~s" (op-name self)))]
-  #:methods gen:equal+hash
-  [(define (equal-proc op1 op2 eq-proc) (eq? op1 op2))
-   (define (hash-proc op1 hash-proc) (op-h1 op1))
-   (define (hash2-proc op2 hash-proc) (op-h2 op2))])
+  [(define (write-proc self port mode) (fprintf port "~s" (op-name self)))])
 
 (define (make-op #:unsafe unsafe #:safe [safe unsafe] #:type type #:name [name (object-name unsafe)] )
   (let ([str-name (format "~s" name)]) 
     (op 
      (string->symbol str-name) 
-     (equal-hash-code str-name) 
-     (equal-secondary-hash-code str-name)
      safe unsafe type)))
 
 (define-syntax-rule (define-operator id arg ...)
@@ -35,4 +29,4 @@
 
 (define (op-out-type operator args) 
   (match operator
-    [(op _ _ _ _ _ t)  (apply t args)]))
+    [(op _ _ _ t)  (apply t args)]))
