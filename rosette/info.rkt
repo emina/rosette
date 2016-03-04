b5972443b98b550569e940de8bad58954d53791f
Leif Andersen <leif@leifandersen.net>
2016-03-04 16:04:21 -0500

    Add info.rkt files.

--- rosette/info.rkt
+++ rosette/info.rkt
@@ -1,4 +1,7 @@
 #lang info
 
+;; Runs the code in `private/install.rkt` before installing this collection.
+
+
 (define pre-install-collection "private/install.rkt")
 (define compile-omit-files '("private/install.rkt"))
