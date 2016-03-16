062414d9f5472f726d10dcf278e1c847531ad184
Emina Torlak <emina@alum.mit.edu>
2016-03-16 09:21:50 -0700

    Disable package documenation compilation (until Scribble bug is fixed).

--- rosette/info.rkt
+++ rosette/info.rkt
@@ -2,7 +2,7 @@
 
 (define collection 'use-pkg-name)
 
-(define deps '("base" "racket"))
+(define deps '("base"))
 (define build-deps '("scribble-lib"
                      "racket-doc"))
 (define compile-omit-paths '("sdsl"
@@ -11,17 +11,17 @@
 (define pkg-desc "Rosette solver-aided host language")
 (define version "2")
 
-;; The format for `scribblings` is documented here:
-;;    http://docs.racket-lang.org/raco/setup-info.html
-(define scribblings
-  '(("doc/guide/scribble/rosette-guide.scrbl"
-     ;; Path to the main documentation file
-     ;; Note: everyone's .scrbl files share the same namespace,
-     ;;       so a name like `guide.scrbl` conflicts with The Racket Guide.
-     ()
-     ;; List of flags for building docs
-     (experimental))))
-     ;; Documentation category. On Racket 6.3+ this can be any string.
+;;; The format for `scribblings` is documented here:
+;;;    http://docs.racket-lang.org/raco/setup-info.html
+;(define scribblings
+;  '(("doc/guide/scribble/rosette-guide.scrbl"
+;     ;; Path to the main documentation file
+;     ;; Note: everyone's .scrbl files share the same namespace,
+;     ;;       so a name like `guide.scrbl` conflicts with The Racket Guide.
+;     ()
+;     ;; List of flags for building docs
+;     (experimental))))
+;     ;; Documentation category. On Racket 6.3+ this can be any string.
 
 ;; Runs the code in `private/install.rkt` before installing this collection.
 (define pre-install-collection "private/install.rkt")
