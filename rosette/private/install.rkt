42366c8d97239402fee221a5876656e07ff91916
Emina Torlak <emina@alum.mit.edu>
2016-03-24 16:49:08 -0700

    Fix directory-exists? check when installing Z3

--- rosette/private/install.rkt
+++ rosette/private/install.rkt
@@ -14,17 +14,18 @@
 
 
 (define (pre-installer collections-top-path racl-path)
-  (define-values (z3-url z3-extracted-subdir) (get-z3-url))
-  (define z3-port (get-pure-port (string->url z3-url) #:redirections 10))
   (define bin-path (build-path racl-path ".." "bin"))
-  (make-directory* bin-path) ;; Ensure that `bin-path` exists
-  (parameterize ([current-directory bin-path])
-    (unless (directory-exists? "z3")
+  (define z3-path (build-path bin-path "z3"))
+  (unless (file-exists? z3-path)
+    (define-values (z3-url z3-extracted-subdir) (get-z3-url))
+    (define z3-port (get-pure-port (string->url z3-url) #:redirections 10))  
+    (make-directory* bin-path) ;; Ensure that `bin-path` exists
+    (parameterize ([current-directory bin-path])    
       (call-with-unzip z3-port
                        (λ (dir)
-                         (copy-directory/files (build-path dir "z3") (build-path bin-path "z3"))))
+                         (copy-directory/files (build-path dir "z3") z3-path)))
       ;; Unzipping loses file permissions, so we reset the z3 binary here
-      (file-or-directory-permissions (build-path bin-path "z3") #o555))))
+      (file-or-directory-permissions z3-path #o555))))
 
 
 ;; Currently unused, but will be useful if Rosette is using a stable z3 release
