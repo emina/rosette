dd0a28968d176670c7ffdd3a454c289f6ae646bb
Emina Torlak <emina@alum.mit.edu>
2016-04-11 14:38:53 -0700

    Add log-based evaluation for Rosette docs.

--- rosette/doc/guide/scribble/util/lifted.rkt
+++ rosette/doc/guide/scribble/util/lifted.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
-(provide select rosette-evaluator)
+(provide select rosette-evaluator rosette-log-evaluator logfile)
 
 (require 
   (for-label racket racket/generic)
   (only-in rosette rosette union union-contents union?)
-  racket/sandbox
+  racket/sandbox racket/serialize scribble/eval
   (only-in scribble/manual elem racket))
 
 (define lifted? 
@@ -17,23 +17,45 @@
           (add-between (map (lambda (id) (racket #,#`#,id)) 
                             (filter lifted? racket-ids)) ", ")))
 
-(define (rosette-printer v)
-  (match v
-    [(? void?) (void)]
-    [(? custom-write?) 
-     ((custom-write-accessor v) v (current-output-port) 1)]
-    [(? pair?) (printf "'~a" v)]
-    [(? null?) (printf "'()")]
-    [(? symbol?) (printf "'~a" v)]
-    [_  (printf "~a" v)]))
-
 (define (rosette-evaluator [eval-limits #f])
    (parameterize ([sandbox-output 'string]
                   [sandbox-error-output 'string]
                   [sandbox-path-permissions `((execute ,(byte-regexp #".*")))]
                   [sandbox-memory-limit #f]
-                  [sandbox-eval-limits eval-limits]
-                  [current-print rosette-printer])
+                  [sandbox-eval-limits eval-limits])
      (make-evaluator 'rosette/safe)))
 
-                    
+(define logfile
+  (let ([files (make-hash)])
+    (lambda (root)
+      (let ([cnt (hash-ref files root 0)])
+        (hash-set! files root (add1 cnt))
+        (build-path root (format "log~a.txt" cnt))))))
+
+(define (serialize-for-logging v)
+  (match v
+    [(or (? boolean?) (? number?) (? string?) (? void?)) v]
+    [(? box?) (box (serialize-for-logging (unbox v)))]
+    [(? pair?) (cons (serialize-for-logging (car v)) (serialize-for-logging (cdr v)))]
+    [(? list?) (map serialize-for-logging v)]
+    [(? vector?) (list->vector (map serialize-for-logging (vector->list v)))]
+    [(? custom-write?)
+     (let ([output-str (open-output-string)])
+       ((custom-write-accessor v) v output-str 1)
+       (opaque (get-output-string output-str)))]
+    [_ v]))
+
+(serializable-struct opaque (str)
+  #:methods gen:custom-write
+  [(define (write-proc self port mode)
+     (fprintf port "~a" (opaque-str self)))])
+
+(define (serializing-evaluator evaluator)
+  (lambda (expr) (serialize-for-logging (evaluator expr))))
+
+(define (rosette-log-evaluator logfile [eval-limits #f])  
+  (if (file-exists? logfile)
+      (make-log-based-eval logfile 'replay)
+      (parameterize ([current-eval (serializing-evaluator (rosette-evaluator eval-limits))])
+        (make-log-based-eval logfile 'record))))
+
