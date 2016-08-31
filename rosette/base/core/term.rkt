119a99f5160afe3146034ac19b937e7a9d5d2777
Emina Torlak <emina@alum.mit.edu>
2016-08-31 14:39:57 -0700

    Add object-name property to lifted operators.

--- rosette/base/core/term.rkt
+++ rosette/base/core/term.rkt
@@ -124,6 +124,8 @@
 (struct operator (identifier range safe unsafe)
   #:property prop:procedure 
   (struct-field-index safe)
+  #:property prop:object-name
+  (struct-field-index identifier)
   #:methods gen:custom-write
   [(define (write-proc self port mode)
      (fprintf port "~a" (id->string (operator-identifier self))))])
