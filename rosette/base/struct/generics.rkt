eaa42a235057c5c19a1b172e68bbed69a357ba00
Emina Torlak <emina@alum.mit.edu>
2017-06-28 16:20:30 -0700

    Allow keyword arguments in generic methods.

--- rosette/base/struct/generics.rkt
+++ rosette/base/struct/generics.rkt
@@ -3,7 +3,7 @@
 (require (for-syntax (only-in racket/syntax
                               format-id wrong-syntax generate-temporary
                               current-syntax-context)
-                     (only-in syntax/stx stx-car))
+                     (only-in syntax/stx stx-pair? stx-car stx-cdr))
          (only-in racket/generic define-generics)
          (only-in "../form/control.rkt" @if)
          (only-in "../core/bool.rkt" @assert)
@@ -119,14 +119,21 @@
                      "expected a list of arguments with no dotted tail")]))
 
   (define (index-of name-stx formals-stx)
-    (let* ([name (syntax->datum name-stx)]
-           [formals (syntax->datum formals-stx)])
-      (let loop ([i 0] [formals formals])
-        (cond [(not (pair? formals))
-               (wrong-syntax #'other "cannot tell where the generic type is")]
-              [(eq? name (car formals))
-               (datum->syntax name-stx i)]
-              [else (loop (+ i 1) (cdr formals))])))))
+    (let loop ([i 0] [formals formals-stx])
+      (unless (stx-pair? formals)
+        (wrong-syntax
+         formals-stx
+         "did not find the generic name ~a among the required, by-position arguments"
+         (syntax->datum name-stx)))
+      (define c (stx-car formals))
+      (cond [(identifier? c)
+             (if (free-identifier=? name-stx (stx-car formals))
+                 (datum->syntax name-stx i)
+                 (loop (+ i 1) (stx-cdr formals)))]
+            [(keyword? (syntax->datum c)) ; count only by-position required arguments
+             (loop i (stx-cdr (stx-cdr formals)))]
+            [else
+             (wrong-syntax c "required arguments must precede optional arguments")]))))
 
 (define-syntax (@define-generics stx)
   (syntax-case stx ()
@@ -169,15 +176,31 @@
          (syntax/loc stx
            (void)))]))
 
-(define-syntax-rule (lift proc receiver-index)
-  (let ([proc proc])
-    (procedure-rename
-     (lambda args
-       (define receiver (list-ref args receiver-index))
-       (if (union? receiver)
-           (for/all ([r receiver]) (apply proc (list-set args receiver-index r)))
-           (apply proc args)))
-     (or (object-name proc) 'proc))))
+(define (lift proc receiver-index)
+  (define-values (required-kws allowed-kws) (procedure-keywords proc))
+  (define arity (procedure-arity proc))
+  (procedure-rename
+   (if (null? allowed-kws)
+       (procedure-reduce-arity
+        (lambda args
+          (define receiver (list-ref args receiver-index))
+          (if (union? receiver)
+              (for/all ([r receiver])
+                (apply proc (list-set args receiver-index r)))
+              (apply proc args)))
+        arity)
+       (procedure-reduce-keyword-arity
+        (make-keyword-procedure
+         (lambda (kws kw-args . args)
+           (define receiver (list-ref args receiver-index))
+           (if (union? receiver)
+               (for/all ([r receiver])
+                 (keyword-apply proc kws kw-args (list-set args receiver-index r)))
+               (keyword-apply proc kws kw-args args))))
+        arity
+        required-kws
+        allowed-kws))
+   (or (object-name proc) 'lifted)))
 
 #|
 ; sanity check
