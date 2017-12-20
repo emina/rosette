5dd866a5b79b8f12996f9e7a14b8c40a80e82832
Emina Torlak <emina@alum.mit.edu>
2017-12-20 14:05:58 -0800

    Suppress the z3 warning message on the Racket package server.

--- rosette/solver/smt/z3.rkt
+++ rosette/solver/smt/z3.rkt
@@ -23,7 +23,7 @@
       (let ([z3.exe-path (path-replace-suffix z3-path ".exe")])
         (if (file-exists? z3.exe-path)
           z3.exe-path
-          (begin
+          (unless (getenv "PLT_PKG_BUILD_SERVICE")
             (printf "warning: could not find z3 executable in '~a'"
                     (path->string (simplify-path (path->directory-path z3.exe-path))))
             z3-path)))))
