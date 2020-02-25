67802efd751fc7fb9203668c840a3a23ae0fdd3f
Emina Torlak <emina@muaddib.cseresearch.cs.washington.edu>
2020-02-24 16:18:45 -0800

    This closes #144 by renaming doc -> guide and simplifying the directory structure.

--- rosette/info.rkt
+++ rosette/info.rkt
@@ -5,7 +5,7 @@
 ;;; The format for `scribblings` is documented here:
 ;;;    http://docs.racket-lang.org/raco/setup-info.html
 (define scribblings
-  '(("doc/guide/scribble/rosette-guide.scrbl"
+  '(("guide/scribble/rosette-guide.scrbl"
      ;; Path to the main documentation file
      ;; Note: everyone's .scrbl files share the same namespace,
      ;;       so a name like `guide.scrbl` conflicts with The Racket Guide.
