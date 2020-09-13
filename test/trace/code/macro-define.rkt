45d659b713ff7bc1d8c94583075ceb2604c18c5a
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2020-09-12 18:26:28 -0700

    lib must be in a separate file to trigger the bug

--- test/trace/code/macro-define.rkt
+++ test/trace/code/macro-define.rkt
@@ -1,13 +1,4 @@
 #lang rosette
 
-(module lib racket
-  (require rosette)
-  (provide mac)
-  (define-syntax-rule (mac)
-    (begin
-      (begin
-        (define foo (verify (1)))
-        (define bar 1)))))
-
-(require 'lib)
+(require "lib.rkt")
 (mac)
