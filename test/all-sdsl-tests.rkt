57034f50d19a54ea9a3d35ae0ccdb887ff943f98
Emina Torlak <emina@alum.mit.edu>
2020-12-10 15:12:19 -0800

    Update websynth to use CVC4 for testing for now.

--- test/all-sdsl-tests.rkt
+++ test/all-sdsl-tests.rkt
@@ -5,7 +5,7 @@
 (error-print-width 4)
 
 (test-groups [test fast] 
- ;"../sdsl/websynth/test/all-tests.rkt"         ; WebSynth
+ "../sdsl/websynth/test/all-tests.rkt"         ; WebSynth
  "../sdsl/bv/test/all-tests.rkt"               ; BV
  (submod "../sdsl/ifc/test.rkt")               ; IFC tests
  (submod "../sdsl/synthcl/test/all-tests.rkt") ; SynthCL tests
